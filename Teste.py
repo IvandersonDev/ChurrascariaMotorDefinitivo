8926b5f1592159c6c2d22c45482d2a822b208953
IvandersonDev <ivandersonamaral16@gmail.com>
2024-03-07 00:36:11 -0300

    Integraçao com Banco

--- Teste.py
+++ Teste.py
@@ -1,12 +1,3 @@
-import dataanalise as da
-import plotly.express as px
+import dao
 
-data = da.analisar2()
-
-data.drop(columns='municipio', inplace=True)
-print(data.corr())
-
-
-fig = px.imshow(data.corr())
-
-fig.show()
+dao.inserirDB('jose@gmail.com','551', dao.conectardb())
